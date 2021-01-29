65d73c4a1910a9ccb4fd41c3a2727c65e36ce865
Senay Yakut <senaykt@gmail.com>
2021-01-29 10:55:08 -0800

    Add capturing rainwater problem

---
+++ hard/capturingRainwater/script.js
@@ -0,0 +1,5 @@
+/*The Problem
+
+Imagine a very heavy rainstorm over a highway that has many potholes and cracks. The rainwater will collect in the empty spaces in the road, creating puddles. Each puddle can only be as high as the road around it, as any excess water will just flow away.
+
+The capturing rainwater problem asks you to calculate how much rainwater would be trapped in the empty spaces in a histogram (a chart which consists of a series of bars).*/
