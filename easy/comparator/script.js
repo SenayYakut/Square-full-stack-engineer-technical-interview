e0d438ca10db93a7fe92ae40c6ea071ad04cb827
Senay Yakut <senaykt@gmail.com>
2021-02-02 10:04:32 -0800

    Add solution for comparator function

--- easy/comparator/script.js
+++ easy/comparator/script.js
@@ -1,1 +1,13 @@
 /*create a comparator function in JavaScript.*/
+
+//Ascending order 
+const ascendingOrder = (a, b) => {
+        return a - b;
+    }
+    // Desending order
+const ascendingOrder = (a, b) => {
+    return b - a;
+}
+const testArray = [10, 43, 5, 0, -2, -20, 4, 3, 2, 1, 11];
+testArray.sort(ascendingOrder);
+console.log(testArray);
